9e55cef4ca1e49d5e210fa1089ce9f895cb7ffd0
Zurlex <arsenovdaniil6@gmail.com>
2025-07-30 15:34:54 +0400

    Upload

---
+++ main.py
@@ -0,0 +1,40 @@
+from contextlib import asynccontextmanager
+
+from pydantic import BaseModel
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from models import init_db
+import requests as rq
+
+
+@asynccontextmanager
+async def lifespan(app_: FastAPI):
+    await init_db()
+    print('Bot is ready')
+    yield
+
+
+app = FastAPI(title="To do App", lifespan=lifespan)
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
+@app.get("/api/tasks/{tg_id}")
+async def tasks(tg_id: int):
+    user = await rq.add_user(tg_id)
+    return await rq.get_task(user.id)
+
+@app.get("/api/main/{tg_id}")
+async def profile(tg_id: int):
+    user = await rq.add_user(tg_id)
+    completed_task_count = await rq.get_completed_task_count(user.id)
+    return {'completed_task_count': completed_task_count}
+
